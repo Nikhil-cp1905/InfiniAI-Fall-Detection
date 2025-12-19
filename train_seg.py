546ba15918023f3621abc77835a5a49137e11670
Nikhil-cp1905 <cpnikhil05@gmail.com>
2025-12-19 15:23:53 +0530

    yoloe not needed for now

---
+++ train_seg.py
@@ -0,0 +1,13 @@
+from ultralytics import YOLO
+
+model = YOLO("yolov8s-seg.pt")
+
+model.train(
+    data="data.yaml",
+    epochs=100,
+    imgsz=640,
+    batch=8,
+    device=0,
+    workers=8
+)
+
